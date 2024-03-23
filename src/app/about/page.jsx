bf3683d53303e87502d93cd4d5d0fe9c593eaf58
anonymousboy67 <aashishad67@gmail.com>
2024-03-23 11:19:16 +0545

    About page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,36 @@
+import Styles from './about.module.css'
 const AboutPage=()=>{
     return(
-        <div>AboutPage</div>
+        <div className={Styles.container}>
+      <div className={Styles.textContainer}>
+        <h2 className={Styles.subtitle}>About Agency</h2>
+        <h1 className={Styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={Styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We’re world’s Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.
+        </p>
+        <div className={Styles.boxes}>
+          <div className={Styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={Styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={Styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
+     
+     
+    </div>
     )
 }
 export default AboutPage
